2aac4533bd875349ba710ceb8e8dd61a8c3c5e16
Anderson Carlos Ferreira da Silva <andersoncarlosfs@outlook.com>
2025-01-15 03:42:30 +0900

    fix error messages

--- sources/Pelias.NET/Model/Objects/Pelias/Converters/BoundingBoxConverter.cs
+++ sources/Pelias.NET/Model/Objects/Pelias/Converters/BoundingBoxConverter.cs
@@ -7,108 +7,34 @@ using System.Text.Json.Serialization;
 
 namespace Pelias.NET.Model.Objects.Pelias.Converters
 {
-    /// <summary>
-    /// Custom <see cref="JsonConverter{T}"/> for converting a <see cref="BoundingBox"/> to and from JSON.
-    /// </summary>
-    public class BoundingBoxConverter : JsonConverter<BoundingBox>
+    public class BoundingBoxConverter : AnglesConverter
     {
-        /// <summary>
-        /// Reads JSON and converts it into a <see cref="BoundingBox"/> object.
-        /// </summary>
-        /// <param name="reader">The <see cref="Utf8JsonReader"/> instance used to read the JSON data.</param>
-        /// <param name="typeToConvert">The type to convert to (expected to be <see cref="BoundingBox"/>).</param>
-        /// <param name="options">Additional options that may control the serialization/deserialization process.</param>
-        /// <returns>A <see cref="BoundingBox"/> instance representing the deserialized JSON data.</returns>
-        public override BoundingBox Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public BoundingBoxConverter() : base(4)
         {
-            // Start the recursive parsing process and return the resulting BoundingBox
-            return GetBoundingBox(ref reader, JsonTokenType.Number, new List<double>(), 4);
         }
 
-        /// <summary>
-        /// Recursively reads and builds a <see cref="BoundingBox"/> object from JSON.
-        /// </summary>
-        /// <param name="reader">The <see cref="Utf8JsonReader"/> instance used to read the JSON data.</param>
-        /// <param name="expectedTokenType">The expected token type for the values in the bounding box (typically <see cref="JsonTokenType.Number"/>).</param>
-        /// <param name="angles">A list of angles (coordinates) being parsed, initially empty.</param>
-        /// <param name="limit">An optional limit to restrict the number of coordinates (usually 4 for bounding boxes).</param>
-        /// <returns>A <see cref="BoundingBox"/> instance built from the parsed values.</returns>
-        /// <exception cref="TypeMismatchException">Thrown if the expected token type does not match the actual token type.</exception>
-        /// <exception cref="CollectionIterationException">Thrown if the number of coordinates exceeds the specified limit.</exception>
-        private BoundingBox GetBoundingBox(ref Utf8JsonReader reader, JsonTokenType expectedTokenType, IList<double> angles, int? limit)
+        public new BoundingBox Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            // Read the next token
-            reader.Read();
+            var angles = base.Read(ref reader, typeToConvert, options);
 
-            // Get the current token type
-            var currentTokenType = reader.TokenType;
-
-            // If we have reached the end of the array, build the BoundingBox object
-            if (currentTokenType == JsonTokenType.EndArray)
+            return new BoundingBox
             {
-                // Ensure the expected four coordinates are parsed
-                if (angles.Count != 4)
+                TopRightCoordinates = new Coordinates
                 {
-                    throw new CollectionIterationException(string.Format(ExceptionsResources.CollectionIterationException, nameof(angles), angles.Count, limit));
+                    Latitude = angles[1],
+                    Longitude = angles[0]
+                },
+                BottomLeftCoordinates = new Coordinates
+                {
+                    Latitude = angles[3],
+                    Longitude = angles[2]
                 }
-
-                return new BoundingBox
-                {
-                    TopRightCoordinates = new Coordinates
-                    {
-                        Latitude = new Angle(angles[1]),
-                        Longitude = new Angle(angles[0])
-                    },
-                    BottomLeftCoordinates = new Coordinates
-                    {
-                        Latitude = new Angle(angles[3]),
-                        Longitude = new Angle(angles[2])
-                    }
-                };
-            }
-
-            // Ensure that the token is the expected type
-            if (currentTokenType != expectedTokenType)
-            {
-                throw new TypeMismatchException(string.Format(ExceptionsResources.TypeMismatchException_NotEqual_JsonTokenType, nameof(reader), currentTokenType,
-                        reader.TokenStartIndex, expectedTokenType, nameof(expectedTokenType)));
-            }
-
-            // If a limit is set, ensure that the number of coordinates does not exceed it
-            if (limit.HasValue && angles.Count >= limit)
-            {
-                throw new CollectionIterationException(string.Format(ExceptionsResources.CollectionIterationException, nameof(angles), angles.Count, limit));
-            }
-
-            // Add the current value (coordinate) to the angles list
-            angles.Add(reader.GetDouble());
-
-            // Continue the recursive process
-            return GetBoundingBox(ref reader, expectedTokenType, angles, limit);
+            };
         }
 
-        /// <summary>
-        /// Writes a <see cref="BoundingBox"/> object to JSON.
-        /// </summary>
-        /// <param name="writer">The <see cref="Utf8JsonWriter"/> instance used to write the JSON data.</param>
-        /// <param name="value">The <see cref="BoundingBox"/> instance to serialize to JSON.</param>
-        /// <param name="options">Additional options that may control the serialization process.</param>
-        public override void Write(Utf8JsonWriter writer, BoundingBox value, JsonSerializerOptions options)
+        public new void Write(Utf8JsonWriter writer, BoundingBox value, JsonSerializerOptions options)
         {
-            // Start writing the JSON array for the bounding box coordinates
-            writer.WriteStartArray();
-
-            // Write each coordinate of the bounding box
-            foreach (var coordinates in value)
-            {
-                foreach (var angle in coordinates)
-                {
-                    writer.WriteNumberValue(angle.Degrees);
-                }
-            }
-
-            // End the JSON array
-            writer.WriteEndArray();
+            base.Write(writer, value.SelectMany(e => e.ToList()).ToList(), options);
         }
     }
 }
--- sources/Pelias.NET/Model/Objects/Pelias/Converters/CoordinatesConverter.cs
+++ sources/Pelias.NET/Model/Objects/Pelias/Converters/CoordinatesConverter.cs
@@ -1,111 +1,28 @@
-﻿using Pelias.NET.Model.Exceptions;
-using Pelias.NET.Model.Objects.Pelias.GeographicInformationSystems;
-using Pelias.NET.Model.Objects.Pelias.GeographicInformationSystems.Measurements.Measures;
-using Pelias.NET.Model.Resources;
+﻿using Pelias.NET.Model.Objects.Pelias.GeographicInformationSystems;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace Pelias.NET.Model.Objects.Pelias.Converters
 {
-    /// <summary>
-    /// A custom <see cref="JsonConverter{T}"/> for serializing and deserializing <see cref="Coordinates"/> objects.
-    /// </summary>
-    public class CoordinatesConverter : JsonConverter<Coordinates>
+    public class CoordinatesConverter : AnglesConverter
     {
-        /// <summary>
-        /// Recursively reads a JSON array and builds a <see cref="Coordinates"/> object.
-        /// </summary>
-        /// <param name="reader">The reader to read the JSON content.</param>
-        /// <param name="expectedTokenType">The expected JSON token type (typically <see cref="JsonTokenType.Number"/> for coordinates).</param>
-        /// <param name="angles">A list of parsed angle values (latitude and longitude).</param>
-        /// <param name="limit">The maximum number of elements to parse (typically 2 for latitude and longitude).</param>
-        /// <returns>A <see cref="Coordinates"/> object.</returns>
-        /// <exception cref="TypeMismatchException">Thrown when the JSON token type doesn't match the expected type.</exception>
-        /// <exception cref="CollectionIterationException">Thrown when the number of parsed angles exceeds the limit.</exception>
-        private Coordinates GetCoordinates(ref Utf8JsonReader reader, JsonTokenType expectedTokenType, IList<double> angles, int? limit = null)
+        public CoordinatesConverter() : base(2)
         {
-            reader.Read(); // Move to the next token
-
-            // Check if we've reached the end of the array
-            if (reader.TokenType == JsonTokenType.EndArray)
-            {
-                // Ensure the expected two coordinates are parsed
-                if (!limit.HasValue || angles.Count == limit.Value)
-                {
-                    // Return a new Coordinates object created from the parsed angles
-                    return new Coordinates
-                    {
-                        Latitude = new Angle(angles[1]),
-                        Longitude = new Angle(angles[0])
-                    };
-                }
-            }
-
-            // Ensure the token type matches the expected one
-            if ((reader.TokenType != expectedTokenType) && (!limit.HasValue || (angles.Count != limit.Value)))
-            {
-                throw new TypeMismatchException(
-                    string.Format(
-                        ExceptionsResources.TypeMismatchException_NotEqual_JsonTokenType,
-                        nameof(reader),
-                        reader.TokenType,
-                        reader.TokenStartIndex,
-                        expectedTokenType,
-                        nameof(expectedTokenType)
-                    )
-                );
-            }
-
-            // Check if the number of parsed angles exceeds the limit
-            if (limit.HasValue && ((angles.Count >= limit.Value) || ((reader.TokenType != expectedTokenType) && (angles.Count != limit.Value))))
-            {
-                throw new CollectionIterationException(
-                    string.Format(
-                        ExceptionsResources.CollectionIterationException,
-                        nameof(angles),
-                        angles.Count,
-                        limit.Value
-                    )
-                );
-            }
-
-            // Add the current token value to the angles list
-            angles.Add(reader.GetDouble());
-
-            // Continue recursively to process the next coordinate
-            return GetCoordinates(ref reader, expectedTokenType, angles, limit);
         }
 
-        /// <summary>
-        /// Reads the JSON and converts it to a <see cref="Coordinates"/> object.
-        /// </summary>
-        /// <param name="reader">The reader to read the JSON content.</param>
-        /// <param name="typeToConvert">The target type for conversion, which is <see cref="Coordinates"/> in this case.</param>
-        /// <param name="options">Options for the JSON serializer.</param>
-        /// <returns>A <see cref="Coordinates"/> object parsed from the JSON.</returns>
-        public override Coordinates Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public new Coordinates Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            // Begin parsing the coordinates, expecting two values (latitude and longitude)
-            return GetCoordinates(ref reader, JsonTokenType.Number, new List<double>(), 2);
+            var angles = base.Read(ref reader, typeToConvert, options);
+
+            return new Coordinates()
+            {
+                Latitude = angles[1],
+                Longitude = angles[0]
+            };
         }
 
-        /// <summary>
-        /// Converts a <see cref="Coordinates"/> object to JSON.
-        /// </summary>
-        /// <param name="writer">The writer to write the JSON content.</param>
-        /// <param name="value">The <see cref="Coordinates"/> object to serialize.</param>
-        /// <param name="options">Options for the JSON serializer.</param>
-        public override void Write(Utf8JsonWriter writer, Coordinates value, JsonSerializerOptions options)
+        public new void Write(Utf8JsonWriter writer, Coordinates value, JsonSerializerOptions options)
         {
-            writer.WriteStartArray(); // Begin the JSON array
-
-            // Write each coordinate value (latitude and longitude)
-            foreach (var angle in value)
-            {
-                writer.WriteNumberValue(angle.Degrees); // Write each coordinate as a number
-            }
-
-            writer.WriteEndArray(); // End the JSON array
+            base.Write(writer, value.ToList(), options);
         }
     }
 }
